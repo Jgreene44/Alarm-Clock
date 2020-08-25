1cd268d72f9ea1d67bcd525ed77687ea206827f8
Jordan Greene <greenjor000@gmail.com>
2020-08-25 00:37:01 -0500

    updated names

--- Alarm501/Alarm501.Designer.cs
+++ Alarm501/Alarm501.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Alarm501
 {
-    partial class Form1
+    partial class Alarm501
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Alarm501
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Alarm501";
+            this.Text = "Alarm 501";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
